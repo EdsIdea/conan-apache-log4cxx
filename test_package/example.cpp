82529b52cb428f5e4fa73d949b23b766bac90598
Ed Clark <ed.clark@gliacelltechnologies.com>
2017-08-16 07:11:59 -0400

    Fixed test exmaple.cpp

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -2,6 +2,10 @@
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
 
+#ifndef EXIT_SUCCESS
+#define EXIT_SUCCESS 0
+#endif
+
 int main() {
     log4cxx::BasicConfigurator::configure();
     LOG4CXX_DEBUG(log4cxx::Logger::getRootLogger(), "Hello, World!");
